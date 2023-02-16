6ac12de43485a93f611da240536f36ca77b91642
Tamanna Chowdhury <tamanna.chowdhury610@gmail.com>
2023-02-16 18:17:42 -0500

    updated file

--- main.cpp
+++ main.cpp
@@ -13,19 +13,26 @@ storage data and will compare data from a certain time period with in the year
 #include <iostream>
 
 int main() {
+  //TASK A
   std::cout << "East Storage Date 05/20/2018: "
             << get_east_storage("05/20/2018")
             << std::endl; // this wont take user input. it will just just pull
-                          // the data from 5/20/2018
+  std::cout << "East Storage Date 06/20/2018: "
+            << get_east_storage("06/20/2018")
+            << std::endl; // this wont take user input. it will just just pull
+  //TASK B
+// the data from 5/20/2018
   std::cout << "Minimum East Basin Storage: " << get_min_east()
             << std::endl; // will show the minimum of the east basin
   std::cout << "Maximum East Basin Storage: " << get_max_east()
             << std::endl; // will show the maximun of the east basin
+  //TASK C 
   std::string compare = compare_basins("09/15/2018");
   std::cout << "Comparing Basins Data 09/15/2018: " << compare
             << std::endl; // this will compare the basins data
+
+  //TASK D
   reverse_oreder("05/29/2018", "06/02/2018");
 
   return 0;
 }
-
